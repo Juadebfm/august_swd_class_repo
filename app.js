0120a8273e8f2f22ac43640a052a585bbe7f2d18
Godspower <amriccygodspea.com>
2024-11-17 17:06:20 +0100

    feat: made changes

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const body = document.body;
 const myDiv = document.createElement('div');
 const myH1 = document.createElement('h1');
 const myImg = document.createElement('img');
-body.appendChild(myDiv);
-myDiv.appendChild(myH1);
-myDiv.appendChild(myImg);
 
-myDiv.style.backgroundColor = '#0000ff';
+myDiv.classList.add("myDiv");
+
 
 myH1.textContent = 'Hello, World!';
-myH1.style.color = 'white';
 
-myImg.src = 'https://picsum.photos/200/300';
+myH1.classList.add("myH1")
+myH1.style.color = 'black';
+
+myImg.classList.add("myImg");
+myImg.src = 'https://images.pexels.com/photos/28847012/pexels-photo-28847012/free-photo-of-stunning-view-of-brussels-town-hall-in-belgium.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2 ';
+
+
+body.appendChild(myDiv);
+myDiv.appendChild(myH1);
+myDiv.appendChild(myImg); 
