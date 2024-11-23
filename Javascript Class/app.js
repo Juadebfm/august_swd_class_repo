a123b6a7b74435043a6d15fc45b5e503bc66222a
Julius Adebowale <juadebgabriel@gmail.com>
2024-11-23 13:33:16 +0100

    fix

---
+++ Javascript Class/app.js
@@ -0,0 +1,13 @@
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth() + 1;
+const day = now.getDay();
+const date = now.getDate();
+const myDate = now.toLocaleDateString();
+
+// console.log(now);
+// console.log(year);
+// console.log(month);
+// console.log(day);
+// console.log(date);
+console.log(myDate);
