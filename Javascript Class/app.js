1c231e508dd5b0f77326fab33148f6cf4645ea0e
Julius Adebowale <juadebgabriel@gmail.com>
2024-12-01 16:57:39 +0100

    feat: Completed the number generator app

--- Javascript Class/app.js
+++ Javascript Class/app.js
@@ -16,11 +16,76 @@ button.addEventListener("click", (event) => {
   if (input.value === "") {
     validateText.textContent = "Please enter a value";
     validateText.className = "text-red-500 text-center mb-3 font-bold";
-  } else if (isNaN(inputValue)) {
+  }
+  
+  else if (isNaN(inputValue)) {
     validateText.textContent =
       "Please enter a valid number, not a string or symbol";
     validateText.className = "text-red-500 text-center mb-3 font-bold";
-  } else {
+  } 
+  
+  else if (input.value > 500) {
+    validateText.textContent = "Please enter a number not more than 500";
+    validateText.className = "text-red-500 text-center mb-3 font-bold";
+  } 
+  
+  else if (input.value < 0) {
+    validateText.textContent =
+      "Please enter a positive number not a negative one";
+    validateText.className = "text-red-500 text-center mb-3 font-bold";
+  } 
+  
+  else {
     validateText.textContent = "";
+    numbersContainer.innerHTML = "";
+    // write a loop that generates numbers
+    for (let i = 1; i <= inputValue; i++) {
+      const span = document.createElement("span");
+      span.textContent = i;
+      // style the span
+      const commonClasses = [
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "border-2",
+        "rounded",
+      ];
+
+      // style for even or odd numbers
+      const colorClasses =
+        i % 2 === 0
+          ? ["border-green-500", "text-green-500"]
+          : ["border-red-500", "text-red-500"];
+
+      //add all classes to the span created
+      span.classList.add(...commonClasses, ...colorClasses);
+
+      // Store the number spans in the localstorage
+      numbersContainer.appendChild(span);
+    }
+
+    //store the number spans in the local storage
+    localStorage.setItem("numberSpans", numbersContainer.innerHTML);
+    // innerHtml
   }
+});
+
+// show saved number spans at page load
+window.addEventListener("load", () => {
+  const storedSpans = localStorage.getItem("numberSpans");
+  if (storedSpans) {
+    numbersContainer.innerHTML = storedSpans;
+  }
+});
+
+// clear validation text when user starts typing
+input.addEventListener("input", () => {
+  validateText.textContent = "";
+});
+
+//Clear the input field on page refresh
+window.addEventListener("beforeunload", () => {
+  input.value = "";
 });
